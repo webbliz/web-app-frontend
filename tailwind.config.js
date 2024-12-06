a202b7a8437d7d96370de8268eb06550d320f4df
jesus <jesus@webbliz.com>
2024-12-06 04:12:48 +0000

    vite.config file fixed to enable styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+// module.exports = {
+//   content: [
+//     "./index.html", 
+//     "./src/**/*.{js,ts,jsx,tsx,html}", 
+//     "./public/**/*.{js,ts,jsx,tsx,html}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx,html}", 
-    "./public/**/*.{js,ts,jsx,tsx,html}",
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,38 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+// import AutoImport from "unplugin-auto-import/vite";
+// import Components from "unplugin-vue-components/vite";
+// import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+// import tailwindcss from "tailwindcss";
+// import autoprefixer from "autoprefixer";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss,
+//     autoprefixer,
+//     AutoImport({ resolvers: [ElementPlusResolver()], }),
+//     Components({ resolvers: [ElementPlusResolver()], }),
+//   ],
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss,
-    autoprefixer,
-    AutoImport({ resolvers: [ElementPlusResolver()], }),
-    Components({ resolvers: [ElementPlusResolver()], }),
-  ],
-});
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
+})
